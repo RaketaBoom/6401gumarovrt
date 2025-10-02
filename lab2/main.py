454ef8da87745380209479cdf637b7c14c7dfe64
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2025-10-02 10:11:20 +0400

    add catImageProcessor

---
+++ lab2/main.py
@@ -0,0 +1,26 @@
+from lab2.CatImageProcessor import CatImageProcessor
+
+
+def main():
+    try:
+        limit = int(input("Введите количество изображений: "))
+        if limit > 100:
+            print("Максимальное количество изображений за один запрос - 100. Установлено 100.")
+            limit = 100
+
+        processor = CatImageProcessor()
+        api_data = processor.get_json_images(limit)
+
+        if api_data:
+            cat_images = processor.json_to_cat_images(api_data)
+            processed_data = processor.process_images(cat_images)
+            processor.save_images(cat_images, processed_data)
+
+    except ValueError as e:
+        print(f"Ошибка: {e}")
+    except Exception as e:
+        print(f"Неожиданная ошибка: {e}")
+
+
+if __name__ == "__main__":
+    main()
